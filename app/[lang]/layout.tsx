8ba998daadf41a3e0d3183d01edf8bb112a9caa1
Rawa <68058955+RawaMustafa@users.noreply.github.com>
2023-10-28 14:26:58 +0300

    web-bundle

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
-
+// author : Rawa Mustafa www.github.com/RawaMustafa
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
